0666f3af608cc1f1e94311e2011cea5ed66ddfab
vitorlpx <lopessccp.3k@gmail.com>
2026-03-18 17:03:19 -0300

    feat: implementando o AuthController, atualizando RegisterRequestDTO e a entidade de User

--- backend/api/src/main/java/com/br/mastermind/api/dto/RegisterRequestDTO.java
+++ backend/api/src/main/java/com/br/mastermind/api/dto/RegisterRequestDTO.java
@@ -14,18 +14,18 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequestDTO {
-    
-  @NotBlank
+
+  @NotBlank(message = "O nome é obrigatório")
   private String name;
 
   @Email
-  @NotBlank
+  @NotBlank(message = "O email é obrigatório")
   private String email;
 
-  @NotBlank
   @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
   @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
-            message = "A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial")
+            message = "A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere special")
+  @NotBlank(message = "A senha é obrigatória")
   private String password;
 
 }
