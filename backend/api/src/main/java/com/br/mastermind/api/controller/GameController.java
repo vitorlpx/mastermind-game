da07bd2f05cb4696d228379be76f1546753097bd
vitorlpx <lopessccp.3k@gmail.com>
2026-03-19 14:39:06 -0300

    feat: adicionando o game controller

---
+++ backend/api/src/main/java/com/br/mastermind/api/controller/GameController.java
@@ -0,0 +1,38 @@
+package com.br.mastermind.api.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody; // <-- importar o RequestBody do Spring
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.mastermind.api.dto.GuessRequestDTO;
+import com.br.mastermind.api.dto.GuessResponseDTO;
+import com.br.mastermind.api.dto.MatchResponseDTO;
+import com.br.mastermind.api.service.GameService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/game")
+@RequiredArgsConstructor
+public class GameController {
+
+  private final GameService gameService;
+
+  @PostMapping("/start")
+  public ResponseEntity<MatchResponseDTO> startMatch(Authentication authentication) {
+    MatchResponseDTO response = gameService.startMatch(authentication.getName());
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
+  
+  @PostMapping("/guess/{matchId}")
+  public ResponseEntity<GuessResponseDTO> submitGuess(@RequestBody GuessRequestDTO guess, @PathVariable String matchId, Authentication authentication) {
+    GuessResponseDTO response = gameService.submitGuess(matchId, guess);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
+
+}
