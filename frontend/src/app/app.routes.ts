6cb7134eac274e4aab1260590d5811ef2afc1060
vitorlpx <lopessccp.3k@gmail.com>
2026-03-20 16:48:41 -0300

    feat: adicionando o guard e o interceptor do frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./features/login/login.component')
+      .then(m => m.LoginComponent)
+  },
+   {
+    path: 'register',
+    loadComponent: () => import('./features/register/register.component')
+      .then(m => m.RegisterComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/dashboard.component')
+      .then(m => m.DashboardComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'game',
+    loadComponent: () => import('./features/game/game.component')
+      .then(m => m.GameComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'ranking',
+    loadComponent: () => import('./features/ranking/ranking.component')
+      .then(m => m.RankingComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
