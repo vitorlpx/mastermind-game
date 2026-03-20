a2a2c31ef133604f02a8c8ba7030b032cf44f1aa
vitorlpx <lopessccp.3k@gmail.com>
2026-03-20 17:37:06 -0300

    feat: adicionando a tela de login funcional

---
+++ frontend/src/app/features/login/login.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  errorMessage = '';
+  isLoading = false;
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    const { email, password } = this.form.value;
+
+    this.authService.login(email!, password!).subscribe({
+      next: () => this.router.navigate(['/dashboard']),
+      error: () => {
+        this.errorMessage = 'Email ou senha inválidos.';
+        this.isLoading = false;
+      }
+    });
+  }
+}
